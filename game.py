e10dbd19ed01e386cba46b52df49379bf7fdc8ae
abansal22 <86925222+abansal22@users.noreply.github.com>
2021-07-08 20:59:58 -0400

    random computer choice

--- game.py
+++ game.py
@@ -12,9 +12,31 @@ print(x)
 
 # VALIDATE THE USER INPUT
 
+if (x == "rock") or (x == "paper") or (x == "scissors"): # "paper"  "scissors"
+    print("VALID")
+else:
+    print("OOPS, INVALID, PLEASE TRY AGAIN")
+    exit()
+
+print("LATER MESSAGES")
+
+
 # GENERATE A COMPUTER COICE
+# source 
+
+# import random
+
+# foo = ['a', 'b', 'c', 'd', 'e']
+# print(random.choice(foo))
+
+valid_options - ["rock", "paper", "scissors"] # list
+
+c = random.choice(valid_options)
+
+print("COMPTUER CHOSE"))
+
 
 
 # DETERMINE THE WINNER
 
-# DISPLAY THE FINAL CHOICE
+# DISPLAY THE FINAL CHOICE  

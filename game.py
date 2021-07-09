66731595ce908c9359ba02c3ed52b1c8b0719fbb
abansal22 <86925222+abansal22@users.noreply.github.com>
2021-07-08 20:37:25 -0400

    Capture user input

---
+++ game.py
@@ -0,0 +1,20 @@
+# game.py
+
+print("Rock, Paper, Scissors, Shoot!")
+
+
+# ASK FOR A USER INPUT
+# source https://docs.python.org/3/library/functions.html#input
+# s = input('--> ')  
+
+X= input("Please choose one of 'rock, 'paper', 'scissors': ")
+print(x)
+
+# VALIDATE THE USER INPUT
+
+# GENERATE A COMPUTER COICE
+
+
+# DETERMINE THE WINNER
+
+# DISPLAY THE FINAL CHOICE
